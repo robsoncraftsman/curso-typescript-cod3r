b5b6f178aa18243c940c78e73fb4f4c62d3378d4
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-08-26 15:33:30 -0300

    Finalização do capítulo 14 do curso

---
+++ integracao-vue/src/componentes/Contador.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Contador</h1>
+    <h2>{{ valor }}</h2>
+    <h3>{{ getParOuImpar() }}</h3>
+    <button @click="setValor(-1)">Dec</button>
+    <button @click="setValor(1)">Inc</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    valorInicial: {
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      valor: this.valorInicial || 0,
+    };
+  },
+  methods: {
+    setValor(delta: number) {
+      this.valor += delta;
+    },
+    getParOuImpar(): string {
+      return this.valor % 2 === 0 ? 'Par' : 'Impar';
+    },
+  },
+});
+</script>
+
+<style></style>
